fe1094b71f7717c8927605aee26ef432b045af7f
liushb7 <157278402+liushb7@users.noreply.github.com>
2024-04-21 20:45:00 +0800

    Create 315计算右侧小于当前元素的个数_ST.cpp

---
+++ Leetcode/BIT&ST/315计算右侧小于当前元素的个数_ST.cpp
@@ -0,0 +1,96 @@
+class ST{
+private:
+    typedef struct node{
+        int sum;
+        int tag;
+    }Node;
+
+    int n;
+    vector<Node> arr;
+    vector<int> d;
+public:
+    ST(int _n):n(_n), arr(n * 4) {}
+
+    void push_up(int p){
+        arr[p].sum = arr[p*2].sum + arr[p*2 + 1].sum;
+    }
+
+    void build(int p, int l, int r){
+        arr[p].tag = 0;
+        if(l == r){
+            arr[p].sum = 0;
+            return;
+        }
+        int mid = (l + r) >> 1;
+        build(p*2, l, mid);
+        build(p*2 + 1, mid + 1, r);
+        push_up(p);
+    }
+
+    void pd(int p, int l, int r){
+        arr[p].sum += arr[p/2].tag * (r - l + 1);
+        arr[p].tag += arr[p/2].tag;
+    }
+
+    void push_down(int p, int l, int r){
+        int mid = (l + r) >> 1;
+        pd(p*2, l, mid);
+        pd(p*2 + 1, mid + 1, r);
+        arr[p].tag = 0;
+    }
+
+    void update(int nl, int nr, int l, int r, int p, int k){
+        if(nl > nr || l > r) return;
+        if(nl <= l && r <= nr){
+            arr[p].sum += k * (r - l + 1);
+            arr[p].tag += k;
+            return;
+        }
+        push_down(p, l, r);
+        int mid = (l + r) >> 1;
+        if(nl <= mid) update(nl, nr, l, mid, p*2, k);
+        if(nr > mid) update(nl, nr, mid + 1, r, p*2 + 1, k);
+        push_up(p);
+    }
+
+    int query(int nl, int nr, int l, int r, int p){
+        if(nl > nr || l > r) return 0;
+        if(nl <= l && r <= nr) return arr[p].sum;
+        int res = 0;
+        push_down(p, l, r);
+        int mid = (l + r) >> 1;
+        if(nl <= mid) res += query(nl, nr, l, mid, p*2);
+        if(nr > mid) res += query(nl, nr, mid + 1, r, p*2 + 1);
+        return res; 
+    }
+
+    void discrete(vector<int>& nums){
+        d.assign(nums.begin(), nums.end());
+        sort(d.begin(), d.end());
+        d.erase(unique(d.begin(), d.end()), d.end());
+    }
+
+    int getId(int x){
+        return lower_bound(d.begin(), d.end(), x) - d.begin() + 1;
+    }
+};
+
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        vector<int> ans;
+        int MIN = *min_element(nums.begin(), nums.end());
+        int MAX = *max_element(nums.begin(), nums.end());
+        int n = max(abs(MAX), abs(MIN)) * 2 + 10;
+        ST st(n);
+        st.build(1, 1, n);
+        st.discrete(nums);
+        for(int i = nums.size() - 1; i >= 0; i --){
+            int id = st.getId(nums[i]);
+            ans.push_back(st.query(1, id - 1, 1, n, 1));
+            st.update(id, id, 1, n, 1, 1);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
