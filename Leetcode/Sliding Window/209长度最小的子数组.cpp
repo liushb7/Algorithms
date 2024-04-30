204b53522de9b8ee0cf200cf726201d49f124b47
liushb7 <157278402+liushb7@users.noreply.github.com>
2024-04-30 20:20:55 +0800

    Update 209长度最小的子数组.cpp

--- Leetcode/Sliding Window/209长度最小的子数组.cpp
+++ Leetcode/Sliding Window/209长度最小的子数组.cpp
@@ -5,7 +5,8 @@ public:
         int l = 0, ans = INT_MAX, s = 0, n = nums.size();
         for(int r = 0; r < n; r ++){
             s += nums[r];
-          // 满足条件时一直循环， 并更新ans的最小值
+        // 满足条件时一直循环， 并更新ans的最小值
+        // 经过while之s < target， 不再满足题目要求的条件
             while(s >= target){
                 ans = min(ans, r - l + 1);
                 s -= nums[l];
